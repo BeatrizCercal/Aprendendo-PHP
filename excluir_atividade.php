fe4347122b47e98ccea241961c184594225379d6
BeatrizCercal <beatriz_c_cachoeira@estudante.sesisenai.org.br>
2025-09-29 08:48:28 -0300

    adicionei excluir atividade

---
+++ excluir_atividade.php
@@ -0,0 +1,14 @@
+<?php
+require_once('bd.php');
+
+$codigo = $_GET['codigo'];
+$stmt = $conexao->prepare("DELETE FROM atividades WHERE pk_atividade = ? =");
+$stmt->bind_param("i", $codigo);
+if ($stmt->execute()) {
+    header("Location: turmas.php");
+    exit;
+} else {
+    echo "Erro ao excluir atividade:" . $stmt->error;
+}
+$stmt->close();
+$conexao->close();
