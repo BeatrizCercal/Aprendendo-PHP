38d976a6254a89d04be234d393a7c845c986b458
BeatrizCercal <beatriz_c_cachoeira@estudante.sesisenai.org.br>
2025-10-03 08:57:28 -0300

    para aprender

---
+++ index1.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+$erro= '';
+
+if (isset($_SESSION['erro'])){
+$erro = ($_SESSION['erro'] ?? '');
+unset ($_SESSION['erro']);
+};
+
+if (isset($_SESSION['nome_professor']) && ($_SESSION['conectado'])){
+    if ($_SESSION["conectado"] == true);
+    header ("Location: turmas.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h2>Página de Login</h2>
+    <form action="login.php" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <label>Senha:</label>
+        <input type="senha" name=senha" required>
+        <button type="button">Entrar</button>
+    </form>
+</body>
+
+</html>
