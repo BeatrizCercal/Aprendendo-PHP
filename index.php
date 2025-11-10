c2b7b4cb4cda73bc40c78354b541ffdf0a583022
BeatrizCercal <beatriz_c_cachoeira@estudante.sesisenai.org.br>
2025-11-10 09:16:10 -0300

    exclusão de documentos extras

--- index.php
+++ index.php
@@ -25,23 +25,23 @@ if (isset($_SESSION["nome_professor"]) && isset($_SESSION['conectado'])) {
 </head>
 
 <body>
-<h2>Página de Login</h2>
+    <h2>Página de Login</h2>
 
-<form action="login.php" method="POST">
-<label>Email:</label>
-<input type="email" name="email" required>
+    <form action="login.php" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required>
 
-<label>Senha:</label>
-<input type="senha" name="senha" required>
-<input type="submit" value = "Entrar">
+        <label>Senha:</label>
+        <input type="senha" name="senha" required>
+        <input type="submit" value="Entrar">
 
-<?php
-if ($erro) : ?>
-<div><?=($erro) ?></div>
-<? endif; ?>
+        <?php
+        if ($erro) : ?>
+            <div><?= ($erro) ?></div>
+        <? endif; ?>
 
 
-</form>
+    </form>
 
 </body>
 
--- sair1.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('bd.php');
-session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
-?>
